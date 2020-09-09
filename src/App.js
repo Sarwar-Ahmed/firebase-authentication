463f6a174f60a6aba4605dea1be214450bf1c9cd
Sarwar-Ahmed <sarwarahmed109@gmail.com>
2020-09-09 19:27:14 +0600

    done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import firebaseConfig from './firebase.config';
 firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [newUser, setNewUser] = useState(false);
   const [user, setUser] = useState({
     isSignedIn: false,
     name: '',
@@ -17,6 +18,8 @@ function App() {
   })
 
   const provider = new firebase.auth.GoogleAuthProvider();
+  var fbProvider = new firebase.auth.FacebookAuthProvider();
+
   const handleSignIn = () => {
     firebase.auth().signInWithPopup(provider)
     .then(res => {
@@ -37,6 +40,25 @@ function App() {
     })
   }
 
+  const handleFbLogin = () => {
+    firebase.auth().signInWithPopup(fbProvider).then(function(result) {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  }
+
   const handleSignOut = () => {
     firebase.auth().signOut()
     .then(res => {
@@ -44,7 +66,9 @@ function App() {
         isSignedIn: false,
         name: '',
         email: '',
-        photo: ''
+        photo: '',
+        error: '',
+        success: false
       }
       setUser(isSignedOutUser);
     })
@@ -52,32 +76,83 @@ function App() {
       console.log(error);
     })
   }
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    if(newUser && user.email && user.password){
+      firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+      .then(res => {
+        const newUserInfo = {...user};
+        newUserInfo.error = '';
+        newUserInfo.success = true
+        setUser(newUserInfo);
+        updateUserName(user.name);
+      })
+      .catch(error => {
+        // Handle Errors here.
+        const newUserInfo = {...user};
+        newUserInfo.error = error.message;
+        newUserInfo.success = false;
+        setUser(newUserInfo);
+        // ...
+      });
+    }
 
+    if(!newUser && user.email && user.password){
+      firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+      .then(res => {
+        const newUserInfo = {...user};
+        newUserInfo.error = '';
+        newUserInfo.success = true
+        setUser(newUserInfo);
+        console.log('sign in user info', res.user)
+      })
+      .catch(error => {
+        // Handle Errors here.
+        const newUserInfo = {...user};
+        newUserInfo.error = error.message;
+        newUserInfo.success = false;
+        setUser(newUserInfo);
+        // ...
+      });
+    }
+    e.preventDefault();
   }
   const handleBlur = (e) => {
-    let isFormValid = true;
+    let isFieldValid = true;
     if(e.target.name === 'email'){
-      isFormValid = /\S+@\S+\.\S+/.test(e.target.value);
+      isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
     }
     if(e.target.name === 'password'){
       const isPasswordValid = e.target.value.length >= 6;
       const passwordHasNumber = /\d{1}/.test(e.target.value);
-      isFormValid = isPasswordValid && passwordHasNumber;
+      isFieldValid = isPasswordValid && passwordHasNumber;
     }
-    if(isFormValid){
+    if(isFieldValid){
       const newUserInfo = {...user};
       newUserInfo[e.target.name] = e.target.value;
       setUser(newUserInfo);
     }
+  }
+
+  const updateUserName = name => {
+    const user = firebase.auth().currentUser;
+
+    user.updateProfile({
+      displayName: name
+    }).then(function() {
+      console.log('user name updated successfully')
+    }).catch(function(error) {
+      console.log(error);
+    });
   }
   return (
     <div className="App">
       {
         user.isSignedIn
         ?<button onClick={handleSignOut}>Sign out</button>
-        :<button onClick={handleSignIn}>Sign in</button>
+        :<button onClick={handleSignIn}>Sign in with Google</button>
       }
+      <br/>
+      <button onClick={handleFbLogin}>Sign in with Facebook</button>
       {
         user.isSignedIn && <div>
           <h2>Welcome, {user.name}</h2>
@@ -86,18 +161,20 @@ function App() {
         </div>
       }
       <h1> Our own Authentication</h1>
-      <p>Name: {user.name}</p>
-      <p>Email: {user.email}</p>
-      <p>Password: {user.password}</p>
+      <input type="checkbox" onChange={() => setNewUser(!newUser)} name="newUser" id=""/>
+      <label htmlFor="newUser">New User Sign up</label>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="name" id="" placeholder="Your Name" onBlur={handleBlur}/>
+        { newUser && <input type="text" name="name" id="" placeholder="Your Name" onBlur={handleBlur}/>}
         <br/>
         <input type="text" name="email" onBlur={handleBlur} placeholder="Enter your email" required/>
         <br/>
         <input type="password" name="password" onBlur={handleBlur} placeholder="Password" required/>
         <br/>
-        <input type="submit" value="Submit"/>
+        <input type="submit" value={newUser ? "Sign up" : "Sign in"}/>
       </form>
+      <p style={{color: 'red'}}>{user.error}</p>
+      { user.success && <p style={{color: 'green'}}>User {newUser ?'Created' : 'Logged In'} Succesfully</p>}
+
 
     </div>
   );
